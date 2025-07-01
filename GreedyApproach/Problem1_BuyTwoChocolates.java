1a186b6cd80633a63e30f6e6b30f6bb469f4f7c3
Aditya gupta <122453566+itsAG003@users.noreply.github.com>
2025-07-01 10:43:24 +0530

    Update Problem1_BuyTwoChocolates.java

--- GreedyApproach/Problem1_BuyTwoChocolates.java
+++ GreedyApproach/Problem1_BuyTwoChocolates.java
@@ -1,3 +1,17 @@
-public class Problem1_BuyTwoChocolates {
-    
+class Solution {
+    public int buyChoco(int[] prices, int money) {
+        int min1 = Integer.MAX_VALUE;
+        int min2 = Integer.MAX_VALUE;
+        for(int p:prices){
+            if(p<min1){
+                min2 = min1;
+                min1 = p;
+            }
+            else if(p<min2){
+                min2 = p;
+            }
+        }
+        int cost = min1 + min2;
+        return (money>=cost) ? money-cost : money;
+    }
 }
